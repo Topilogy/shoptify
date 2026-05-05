b5a9e0f83cc3c49938cc1714c4a6cf00c346ace6
Temitope <topilogy@gmail.com>
2026-05-05 16:28:58 +0100

    third commit

--- shoptify-frontend/src/socket.js
+++ shoptify-frontend/src/socket.js
@@ -4,9 +4,7 @@ const socket = io("socket.io")(server, {
   cors: {
     origin: process.env.CLIENT_URL, 
     methods: ["GET", "POST"]
-  },
-  reconnectionAttempts: 5,
-  transports: ["websocket"],
+  }
 });
 
 export default socket;
