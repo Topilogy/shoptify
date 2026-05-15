a63b86952c8751ae006f610ad9ed9d9e7f6991c4
Temitope <topilogy@gmail.com>
2026-05-15 20:46:28 +0100

    8:46 commit

--- shoptify-backend/routes/chatRoutes.js
+++ shoptify-backend/routes/chatRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Chat = require("../models/Chat2");
+const Chat = require("../models/Chat");
 const authMiddleware = require("../middleware/authMiddleware");
 
 
